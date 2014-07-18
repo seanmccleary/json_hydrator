9f57a85be08cb21ec9b20d8d060724be60d4f0b6
Sean McCleary <sean.mccleary@gmail.com>
2014-07-17 23:49:51 -0400

    Moved classes into separate files

---
+++ lib/json_hydrator.dart
@@ -0,0 +1,8 @@
+library json_hydrator;
+
+import 'dart:mirrors';
+import "dart:convert";
+import "dart:async";
+
+part "src/json_hydrator.dart";
+part "src/key_value_pair.dart";
---
+++ lib/src/key_value_pair.dart
@@ -0,0 +1,8 @@
+part of json_hydrator;
+
+class _KeyValuePair {
+  var key;
+  var value;
+
+  _KeyValuePair(this.key, this.value);
+}
